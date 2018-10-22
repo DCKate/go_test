085d7d52ad98d08fe8aab55abe4deb7d27620951
Kate Hung <kate_hung@KKK-Mac-mini.local>
2018-10-22 16:08:28 +0800

    20181022 Fixed if padding number = 16

--- AES128/padding/p7Padder.go
+++ AES128/padding/p7Padder.go
@@ -16,10 +16,16 @@ func NewP7Padder(blen int) *p7Padder {
 func (se *p7Padder) GoPad(src []byte) []byte {
 	sl := len(src)
 	ll := se.blockLen - (sl % se.blockLen)
+	if ll == 16 {
+		return Padding(src, []byte("g"), se.blockLen)
+	}
 	h := fmt.Sprintf("%x", ll)
 	return Padding(src, []byte(h), se.blockLen)
 }
 func (se *p7Padder) UnPad(src []byte) []byte {
+	if string(src[len(src)-1]) == "g" {
+		return src[:len(src)-16]
+	}
 	nu, _ := strconv.ParseUint(string(src[len(src)-1]), 16, 64)
 	return src[:len(src)-int(nu)]
 }
